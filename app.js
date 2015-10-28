d7b0c2c73f33b3cd5a373736aedb53cc50c9a16d
Melvin Lee <melvinlee@hotmail.com>
2015-10-28 19:49:38 +0800

    Refactor code

--- app.js
+++ app.js
@@ -12,25 +12,35 @@ net.createServer(function (connection) {
     
     console.log("Receive data: " + data);
     
-    if(data.toString().trim() === 'smdr'){
-      console.log("Sending enter password ...");
-      connection.write("Enter Password: ")
-    }
+    if(data.toString().trim() === 'smdr') validate();
+    if(data.toString().trim() === 'PCCSMDR') sendSmdr();
     
-    if(data.toString().trim() === 'PCCSMDR'){
-      console.log("Receive valid password")
-      
-      connection.write("081313 1610 0036 00338 9                             41101        62485755               033                    \n");
-      sleep.sleep(2);
-      connection.write("081313 1652 0005 00034 7  *00 *799                   66000           13537                   116                \n");
-      sleep.sleep(2);
-      connection.write("081313 1726 0000 00000 E                              7392            *791         \n");
-    }
   });
 
   connection.on('end', function () {
+    
     console.log("Client [" + connection.name + "] disconnected.\n")
+    
   });
+  
+  function validate(){
+    
+      console.log("Sending enter password ...");
+      connection.write("Enter Password: ")
+      
+  };
+  
+  function sendSmdr(){
+    
+    console.log("Receive valid password")
+      
+    connection.write("081313 1610 0036 00338 9                             41101        62485755               033                    \n");
+    sleep.sleep(2);
+    connection.write("081313 1652 0005 00034 7  *00 *799                   66000           13537                   116                \n");
+    sleep.sleep(1);
+    connection.write("081313 1726 0000 00000 E                              7392            *791         \n");
+  
+  }
   
 }).listen(port);
 
